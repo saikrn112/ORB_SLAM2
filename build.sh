1e77fe021737d9728faaf573c06fe26d3d8d5a90
ramz <sairkn112@gmail.com@>
2018-05-07 12:37:26 +0530

    changed number of jobs 2

--- build.sh
+++ build.sh
@@ -28,8 +28,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-<<<<<<< HEAD
 make -j3
-=======
-make -j2
->>>>>>> a498495ef6fef4356d6f4707f3090179f01abb68
+
--- build_ros.sh
+++ build_ros.sh
@@ -4,8 +4,4 @@ cd Examples/ROS/ORB_SLAM2
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
-<<<<<<< HEAD
 make -j3
-=======
-make -j2
->>>>>>> a498495ef6fef4356d6f4707f3090179f01abb68
